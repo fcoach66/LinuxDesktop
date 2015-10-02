5fc2f1955dd13a5aced11508bd12ed5a87703da0
fcoach66 <fabrizio.cocciari@hotmail.it>
2015-10-02 08:46:27 +0200

    Mod 1-unity-greeter.sh

--- chaletos/1-unity-greeter.sh
+++ chaletos/1-unity-greeter.sh
@@ -10,31 +10,31 @@ echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 
 
 echo "[com.canonical.unity-greeter]" > /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "background-color='#00B6BD'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "background='/usr/share/wallpapers/kde-default.png'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "background-color='#000000'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "background='/usr/share/themes/ChaletOSRes/splash.png'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "draw-user-backgrounds=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "draw-grid=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "logo='/usr/share/unity-greeter/logo.png'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#background-logo=''" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "theme-name='Ambiance'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#icon-theme-name=" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#font-name=" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#xft-antialias" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#xft-dpi" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#xft-hintstyle=" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
-echo "#xft-rgba=" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+#echo "#background-logo=''" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "theme-name='Greybird_W8'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "icon-theme-name='ChaletOS Emerald'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "font-name='Droid Sans 9'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "xft-antialias=true" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "xft-dpi=96" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "xft-hintstyle='hintnone'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
+echo "xft-rgba='rgb'" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "#onscreen-keyboard=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "#high-contrast=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "#screen-reader=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 echo "play-ready-sound=false" >> /usr/share/glib-2.0/schemas/20_sinfocom-unity-greeter.gschema.override
 glib-compile-schemas /usr/share/glib-2.0/schemas/
-sed -i "s|XftHintStyle=hintmedium|XftHintStyle=hintfull|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|font=Ubuntu,9,-1,5,50,0,0,0,0,0|desktopFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|menuFont=Ubuntu,9,-1,5,50,0,0,0,0,0|fixed=Droid Sans Mono,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|desktopFont=Ubuntu,9,-1,5,50,0,0,0,0,0|font=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|toolBarFont=Ubuntu,8,-1,5,50,0,0,0,0,0|menuFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|smallestReadableFont=Ubuntu,8,-1,5,50,0,0,0,0,0|smallestReadableFont=Droid Sans,8,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|taskbarFont=Ubuntu,9,-1,5,50,0,0,0,0,0|taskbarFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "s|activeFont=Ubuntu,9,-1,5,50,0,0,0,0,0|toolBarFont=Droid Sans,8,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
-sed -i "/XftHintStyle=hintfull/aXftSubPixel=rgb" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|XftHintStyle=hintmedium|XftHintStyle=hintfull|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|font=Ubuntu,9,-1,5,50,0,0,0,0,0|desktopFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|menuFont=Ubuntu,9,-1,5,50,0,0,0,0,0|fixed=Droid Sans Mono,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|desktopFont=Ubuntu,9,-1,5,50,0,0,0,0,0|font=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|toolBarFont=Ubuntu,8,-1,5,50,0,0,0,0,0|menuFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|smallestReadableFont=Ubuntu,8,-1,5,50,0,0,0,0,0|smallestReadableFont=Droid Sans,8,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|taskbarFont=Ubuntu,9,-1,5,50,0,0,0,0,0|taskbarFont=Droid Sans,9,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "s|activeFont=Ubuntu,9,-1,5,50,0,0,0,0,0|toolBarFont=Droid Sans,8,-1,5,50,0,0,0,0,0|g" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
+#sed -i "/XftHintStyle=hintfull/aXftSubPixel=rgb" /usr/share/kubuntu-default-settings/kde4-profile/default/share/config/kdeglobals
 
